7859003fa33f6dcbbe10f1d58bc5364b746b817f
bithighlander <bithighlander@gmail.com>
2024-08-10 22:30:38 -0600

    header

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Flex,
   Avatar,
+  Link,
   AvatarBadge,
   Button,
   Menu,
@@ -23,7 +24,7 @@ const configKeepKey: any = {
   pairingInfo: {
     name: "KeepKey-Template",
     imageUrl:
-      "https://www.keepkey.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkeepkey_logo.407f5aca.png&w=3840&q=100",
+        "https://www.keepkey.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkeepkey_logo.407f5aca.png&w=3840&q=100",
     basePath: spec,
     url: "https://keepkey-template.vercel.app/",
   },
@@ -45,7 +46,7 @@ const checkAndLaunch = async () => {
   if (!(await checkKeepkeyAvailability(spec))) {
     if (attempt === 3) {
       alert(
-        "KeepKey desktop is required for keepkey-sdk, please go to https://keepkey.com/get-started"
+          "KeepKey desktop is required for keepkey-sdk, please go to https://keepkey.com/get-started"
       );
     } else {
       window.location.assign("keepkey://launch");
@@ -58,26 +59,24 @@ const checkAndLaunch = async () => {
 const Header = () => {
   const [keepkeyConnected, setKeepKeyConnected] = useState(false);
   const [keepkeyError, setKeepKeyError] = useState(false);
-  const [features, setKeepKeyFeatures] = useState({});
+  const [features, setKeepKeyFeatures] = useState<any>({});
 
   const onStart = async function () {
     try {
       const apiKey = localStorage.getItem("apiKey") || "1234";
       configKeepKey.apiKey = apiKey;
 
-      // init
       const sdk = await KeepKeySdk.create(configKeepKey);
       localStorage.setItem("apiKey", configKeepKey.apiKey);
 
       const featuresKK = await sdk.system.info.getFeatures();
-      // eslint-disable-next-line no-console
       console.log("features: ", featuresKK);
       if (featuresKK) {
         setKeepKeyConnected(true);
       }
       setKeepKeyFeatures(featuresKK);
     } catch (e) {
-      setKeepKeyError("Bridge is offline!");
+      setKeepKeyError(true);
     }
   };
 
@@ -87,57 +86,65 @@ const Header = () => {
       onStart();
     };
     initialize();
-  }, []); // once on startup
+  }, []);
 
   return (
-    <Flex
-      as="header"
-      width="full"
-      align="center"
-      alignSelf="flex-start"
-      justifyContent="center"
-      gridGap={2}
-    >
-      <Box marginLeft="auto">
-        <Menu>
-          <MenuButton
-            as={Button}
-            rounded="full"
-            variant="link"
-            cursor="pointer"
-            minW={200}
-          >
-            <Avatar size="lg" src={KEEPKEY_ICON}>
-              {keepkeyConnected ? (
-                <AvatarBadge boxSize="1.25em" bg="green.500" />
-              ) : (
-                <AvatarBadge boxSize="1.25em" bg="red.500" />
-              )}
-            </Avatar>
-          </MenuButton>
-          <MenuList>
-            <Accordion defaultIndex={[0]} allowMultiple>
-              <AccordionItem>
-                <h2>
+      <Flex
+          as="header"
+          width="full"
+          align="center"
+          justifyContent="flex-end"
+          padding={4}
+      >
+        <Box>
+          <Menu>
+            <MenuButton
+                as={Button}
+                rounded="full"
+                variant="link"
+                cursor="pointer"
+                minW={0}
+            >
+              <Avatar size="lg" src={KEEPKEY_ICON}>
+                {keepkeyConnected ? (
+                    <AvatarBadge boxSize="1.25em" bg="green.500" />
+                ) : (
+                    <AvatarBadge boxSize="1.25em" bg="red.500" />
+                )}
+              </Avatar>
+            </MenuButton>
+            <MenuList>
+              <Accordion allowToggle>
+                <AccordionItem>
                   <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      <small>features:</small>
+                    <Box flex="1" textAlign="left">
+                      Device Features
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  <small>
-                    version: {features?.major_version}.{features?.minor_version}
-                    .{features?.patch_version}
-                  </small>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </MenuList>
-        </Menu>
-      </Box>
-    </Flex>
+                  <AccordionPanel pb={4}>
+                    {keepkeyError ? (
+                        <small>Bridge is offline!</small>
+                    ) : (
+                        <>
+                          <small>
+                            Version: {features?.major_version}.{features?.minor_version}
+                            .{features?.patch_version}
+                          </small>
+                          <Box mt={2}>
+                            <Link href="http://localhost:1646/docs" isExternal color="teal.500">
+                              API Documentation
+                            </Link>
+                          </Box>
+                        </>
+                    )}
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
   );
 };
 
